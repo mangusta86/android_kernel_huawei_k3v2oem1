dddb17f48bc43355cecab1145bf57832c41978b2
Marco Fassi <fassimarco86@gmail.com>
2015-02-08 00:28:21 +0000

    Port P6 kernel KITKAT

--- hwu9508_BoardConfig.mk
+++
@@ -1,8 +0,0 @@
-USE_DEVICE_U9508 := true
-USE_DEVICE_U9510 := false
-export USE_LCD_CMI_OTM1280A := true
-
-export USE_PRODUCT_U9508 := true
-export USE_PRODUCT_U9510 := false
-
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
